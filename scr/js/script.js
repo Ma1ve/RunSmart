eaea5beddff51f06f42e8a295c6c96847b3c4a16
Ilya <theyakubovi@gmail.com>
2021-11-02 00:04:54 +0300

    icon_go

--- scr/js/script.js
+++ scr/js/script.js
@@ -33,3 +33,42 @@ document.querySelector('.slick-next').addEventListener('click',function () {
   slider.goTo("next");
 })
 
+$(function() {
+  
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+      .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+      .closest('div.container-fixed').find('div.catalog__contend').removeClass('catalog__contend_active').eq($(this).index()).addClass('catalog__contend_active');
+  });
+
+  $('.catalog__view').each(function(i) {
+    $(this).on('click', function(e) {
+      e.preventDefault();
+      $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+      $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+    })
+  })
+
+  $('.catalog-item__back').each(function(i) {
+    $(this).on('click', function(e) {
+      e.preventDefault();
+      $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+      $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+    })
+  })
+
+
+  
+
+
+  /* function toggleSlide(item) {
+    $('.item').each(function(i) {
+    $(this).on('click', function(e) {
+      e.preventDefault();
+      $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+      $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+    })
+  })
+  } */
+
+});
